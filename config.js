b5fa5b2a7f0ff54e3d6987fea95657958c1f3bd3
刘伟坤 <1834183435@qq.com>
2019-07-05 14:45:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,9 @@ const config = {
   client_id: 'test',
   client_secret: 'test',
   //client_appId:'wx56a4557ca00a75a8',
-  // BASE_URL:'https://quxia.renqilai.com'
   //  BASE_URL: 'https://rapapi.renqilai.com/app/mock/35'
   // BASE_URL:'http://192.168.0.252:8080'
   //  BASE_URL: 'http://192.168.0.222:8080'
-  BASE_URL: 'https://quxiaapi.renqilai.com'
 }
 
 export default config;
